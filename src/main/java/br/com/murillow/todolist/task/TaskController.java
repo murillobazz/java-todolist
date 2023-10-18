cd5d4940f800dee8198a960d97f0976f682282ae
murillobazz <murillobazilio@gmail.com>
2023-10-17 21:59:53 -0300

    deploy fix 2

--- src/main/java/br/com/murillow/todolist/task/TaskController.java
+++ src/main/java/br/com/murillow/todolist/task/TaskController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.murillow.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.var;
 
 
 @RestController
